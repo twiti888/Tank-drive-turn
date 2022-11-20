505370fac61239db68ae15477a4b578e7a6c9534
twiti888 <118575207+twiti888@users.noreply.github.com>
2022-11-19 23:19:52 -0500

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.*;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class. If you change the name of this class or the
+ * package after creating this project, you must also update the build.gradle file in the project.
+ */
+public class Robot extends TimedRobot {
+
+  private Spark leftMotor1 = new Spark(0);
+  private Spark leftMotor2 = new Spark(1);
+  private Spark rightMotor1 = new Spark(2);
+  private Spark rightMotor2 = new Spark(3);
+
+  private Joystick joy1 = new Joystick(0);
+
+
+  private double startTime;
+
+  @Override
+  public void robotInit(){
+
+  }
+
+  @Override
+  public void autonomousInit(){
+
+    startTime = Timer.getFPGATimestamp();
+  }
+
+  @Override
+  public void autonomousPeriodic(){
+    
+    double time = Timer.getFPGATimestamp();
+
+    if(time - startTime < 3){ 
+      
+    leftMotor1.set(0.6);
+    leftMotor2.set(0.6);
+    rightMotor1.set(-0.6);
+    rightMotor2.set(-0.6);
+  }
+  else{
+    leftMotor1.set(0);
+    leftMotor2.set(0);
+    rightMotor1.set(0);
+    rightMotor2.set(0);
+
+    }
+  }
+
+  @Override
+  public void teleopInit(){
+
+  }
+  
+  @Override
+  public void teleopPeriodic(){
+
+  double speed = -joy1.getRawAxis(1) *0.6;
+  double turn = joy1.getRawAxis(4) *0.3;
+
+  double left = speed + turn;
+  double right = speed - turn;
+
+  leftMotor1.set(left);
+  leftMotor2.set(left);
+  rightMotor1.set(-right);
+  rightMotor2.set(-right);
+  }
+
+  @Override
+  public void testInit(){
+
+  }
+
+  @Override
+  public void testPeriodic(){
+
+  }
+}
